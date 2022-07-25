f4d84ce495cde09b32b435e2cc5f6d8d28ef5b09
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2022-07-25 11:33:22 +0000

    0.0.4 局を選択できる様になった

--- index.js
+++ index.js
@@ -36,6 +36,9 @@ const ytpl = require('skt-ytpl');
 const ytsr = require("ytsr");
 const validUrl = require("valid-url");
 
+//radijs
+const radijs = require('radijs');
+
 //queue
 const queue = new Map();
 
@@ -76,9 +79,9 @@ client.on("message", async (msg) => {
   if (msg.author.bot) return;
   //console.log("message:", msg.content);
 
-  if (msg.content.match(/^%sr /) || msg.channel.name.match("sr")) {
+  if (msg.content.match(/^!radiko /) || msg.channel.name.match("radiko")) {
     console.log("firstcmd: sr");
-    var secondory_msg = msg.content.replace(/^%sr /, "");
+    var secondory_msg = msg.content.replace(/^!radiko /, "");
 
     var voiceChannel = msg.member.voice.channel;
 
@@ -103,38 +106,11 @@ client.on("message", async (msg) => {
 
       var musicTitle = "";
       var musicUrl = "";
-      var isLive = "";
-      if (validUrl.isUri(messageInfo)) {
-	// musicUrl contains playlist then foreach add
-	if(messageInfo.match(/.*?list=/)){
-          //playlist
-          var playlistId = messageInfo.replace(/.*?list=/,"").replace(/&.*/,"")
-          var playlist = await ytpl(playlistId)
-          await playlist.items.forEachAsync(async(videoInfo) => {
-            musicUrl = videoInfo.shortUrl
-            musicTitle =  await getYoutubeTitle(videoInfo.id)
-            isLive =  await getYoutubeType(videoInfo.id) == "live"
-            addMusics.push({title:musicTitle, url: musicUrl, isLive: isLive})
-	  })
-	} else {
-          //video
-          musicUrl = messageInfo;
-          musicTitle =  await getYoutubeTitle(musicUrl.replace(/.*?v=/,"").replace(/&.*/,""))
-          isLive =  await getYoutubeType(musicUrl.replace(/.*?v=/,"").replace(/&.*/,"")) == "live"
-          addMusics.push({title:musicTitle, url: musicUrl, isLive: isLive})
-	}
-      } else {
-        const options = {
-          pages: 1,
-        };
-        const filters1 = await ytsr.getFilters(messageInfo);
-        const filter1 = filters1.get("Type").get("Video");
-        const searchResults = await ytsr(filter1.url, options);
-        musicTitle = searchResults.items[0].title;
-        musicUrl = searchResults.items[0].url;
-        isLive =  await getYoutubeType(musicUrl.replace(/.*?v=/,"").replace(/&.*/,"")) == "live"
-        addMusics.push({title:musicTitle, url: musicUrl, isLive: isLive})
-      }
+      // musicUrl contains playlist then foreach add
+      musicUrl = await radijs.get_bangumi_uri(messageInfo);
+      chunkUrl = await radijs.get_m3u8(musicUrl, await radijs.get_authtoken())
+      musicTitle =  messageInfo
+      addMusics.push({title:musicTitle, url: musicUrl, chunkUrl: chunkUrl})
 
       if (!queue.get(msg.guild.id)) {
         queue.set(msg.guild.id, {
@@ -146,7 +122,7 @@ client.on("message", async (msg) => {
       }
       
       await addMusics.forEachAsync(async(item) => {
-        queue.get(msg.guild.id).songs.push({ title: item.title, url: item.url, isLive: item.isLive });
+        queue.get(msg.guild.id).songs.push({ title: item.title, url: item.url, chunkUrl: item.chunkUrl});
       })
 
       msg.channel.send("added to queue -> " + messageInfo);
@@ -200,97 +176,12 @@ client.on("message", async (msg) => {
       return;
     }
 
-    // shuffle cmd
-    if (secondory_msg.match(/^sh/) || secondory_msg.match(/^shuffle/)) {
-      for (i = queue.get(msg.guild.id).songs.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i - 1) + 1);
-        var tmp = queue.get(msg.guild.id).songs[i];
-        queue.get(msg.guild.id).songs[i] = queue.get(msg.guild.id).songs[j];
-        queue.get(msg.guild.id).songs[j] = tmp;
-      }
-      msg.channel.send("shuffled");
-
-      queue.get(msg.guild.id).songs;
-      return;
-    }
-
-    // mv cmd
-    if (secondory_msg.match(/^mv .*/)) {
-      console.log("secondcmd: mv");
-      var index = secondory_msg.replace(/^mv /, "") - 1;
-      //have issue #6
-      if (index == 0) {
-        msg.channel.send("1 cannot be specified");
-        return;
-      }
-      console.log(queue.get(msg.guild.id).songs);
-      queue.get(msg.guild.id).songs.move(index, 1);
-      let replacer = function(key,value){
-	if (key == "url") {
-	  return "<" + value + ">"
-	}
-	return value
-      }
-      var songs = JSON.parse(JSON.stringify(queue.get(msg.guild.id).songs, replacer));
-      songs = songs.reduce((prev, current, index) => {
-        prev[index + 1] = current;
-        return prev;
-      }, {})
-
-      msg.channel.send("moved" + "\r " + JSON.stringify(songs, null, "\t"));
-
-      return;
-    }
-
-    // mv cmd
-    if (secondory_msg.match(/^rm .*/)) {
-      console.log("secondcmd: rm");
-      var index = secondory_msg.replace(/^rm /, "") - 1;
-      if (index == 0){
-        return;
-      }
-      queue.get(msg.guild.id).songs.splice(index,1);
-      let replacer = function(key,value){
-	if (key == "url") {
-	  return "<" + value + ">"
-	}
-	return value
-      }
-      var songs = JSON.parse(JSON.stringify(queue.get(msg.guild.id).songs, replacer));
-      songs = songs.reduce((prev, current, index) => {
-        prev[index + 1] = current;
-        return prev;
-      }, {})
-
-      msg.channel.send("removed -> " + (index + 1));
-
-      return;
-    }
-
-    // skip cmd
-    if (secondory_msg.match(/^s$/) || secondory_msg.match(/^skip$/) ) {
-      queue.get(msg.guild.id).connection.dispatcher.end("Skip command used")
-      return
-    }
-
-    // loop cmd
-    if (secondory_msg.match(/^loop$/)) {
-      if (queue.get(msg.guild.id).isLoop) {
-        queue.get(msg.guild.id).isLoop = false
-        msg.channel.send("changed loop status -> off");
-      } else {
-        queue.get(msg.guild.id).isLoop = true
-        msg.channel.send("changed loop status -> on");
-      }
-      return
-    }
-
     // version cmd
     if (secondory_msg.match(/^v/) || secondory_msg.match(/^version/) ) {
       msg.channel.send("bot name : " + name);
       msg.channel.send("version : " + version);
       msg.channel.send("description : " + description);
-      msg.channel.send("How to use : " + "https://github.com/sakkuntyo/discord-sktrythmjs/blob/dependabot/npm_and_yarn/ws-7.5.5/readme.md");
+      msg.channel.send("How to use : " + "https://github.com/sakkuntyo/discord-radicord/blob/dependabot/npm_and_yarn/ws-7.5.5/readme.md");
       msg.channel.send("invitation link : " + "https://discord.com/api/oauth2/authorize?client_id=889584860308570113&permissions=3147904&scope=bot");
       return;
     }
@@ -318,91 +209,11 @@ client.on("message", async (msg) => {
         return 1;
       }
     }
-
-    // search playlist cmd
-    if (secondory_msg.match(/^spl/)) {
-      console.log("secondcmd: search playlist");
-      var keyword = secondory_msg.replace(/^spl /, "");
-      console.log("keyword ->", keyword);
-      let replacer = function(key,value){
-      	if (key == "id") {
-      	  return "<" + "https://www.youtube.com/playlist?list=" + value + ">"
-      	}
-      	return value
-      }
-      playlists = await spl(keyword);
-      console.dir(playlists)
-      playlists = JSON.parse(JSON.stringify(playlists, replacer));
-      playlists = playlists.reduce((prev, current, index) => {
-      	prev[index + 1] = current;
-      	return prev;
-      }, {})
-      msg.channel.send(JSON.stringify(playlists,null,2))
-    }
-    
-    // play playlist cmd
-    if (secondory_msg.match(/^ppl/)) {
-      console.log("secondcmd: play playlist");
-      var indexAndKeyword = secondory_msg.replace(/^ppl /, "");
-      console.log("indexAndkeyword ->", indexAndKeyword);
-      var index = indexAndKeyword.replace(/([1-5]).*/, '$1') - 1;
-      console.log("index ->", index);
-      var keyword = indexAndKeyword.replace(/[1-5] /, '');
-      console.log("keyword ->", keyword);
-      let replacer = function(key,value){
-      	if (key == "id") {
-      	  return "<" + "https://www.youtube.com/playlist?list=" + value + ">"
-      	}
-      	return value
-      }
-      playlists = await spl(keyword);
-      console.dir(playlists)
-　　  playlistId = playlists[index].id
-      console.log("playlistId ->" + playlists[index].id)
-
-      //search and push queue
-      const addMusics = [];
-
-      var musicTitle = "";
-      var musicUrl = "";
-      var isLive = "";
-      //playlist
-      var playlist = await ytpl(playlistId)
-      await playlist.items.forEachAsync(async(videoInfo) => {
-        musicUrl = videoInfo.shortUrl
-        musicTitle =  await getYoutubeTitle(videoInfo.id)
-        isLive =  await getYoutubeType(videoInfo.id) == "live"
-        addMusics.push({title:musicTitle, url: musicUrl, isLive: isLive})
-      })
-
-      if (!queue.get(msg.guild.id)) {
-        queue.set(msg.guild.id, {
-          playing: false,
-          connection: null,
-          songs: [],
-          isLoop: false,
-        });
-      }
-      
-      await addMusics.forEachAsync(async(item) => {
-        queue.get(msg.guild.id).songs.push({ title: item.title, url: item.url, isLive: item.isLive });
-      })
-
-      msg.channel.send("added to queue -> " + (parseInt(index) + 1) + " " + keyword);
-      msg.channel.send("queue length -> " + queue.get(msg.guild.id).songs.length);
-      
-      //already playing
-      if (queue.get(msg.guild.id).playing) {
-        console.log("Since it is playing, we will just add it to the queue.");
-        return;
-      }
-
-      play(msg, queue.get(msg.guild.id).songs[0]);
-    }
   }
 });
 
-function play(msg) {
+async function play(msg) {
+  
   if (!queue.get(msg.guild.id).songs[0]) {
     console.log("tried to play, but the queue was empty, so I quit.");
     msg.guild.me.voice.channel.leave();
@@ -410,12 +221,11 @@ function play(msg) {
   }
 
   var stream = null;
-  if(queue.get(msg.guild.id).songs[0].isLive){
-    stream = ytdl(queue.get(msg.guild.id).songs[0].url, "dfImprmy_fuB4YzmZGH6lA", {
-      opusEncoded: true,
-      encoderArgs: ["-af", "bass=g=3,volume=0.05"],
-    });
-  }
+  //var token = await radijs.get_authtoken();
+  stream = ytdl(queue.get(msg.guild.id).songs[0].chunkUrl, await radijs.get_authtoken(), {
+    opusEncoded: true,
+    encoderArgs: ["-af", "bass=g=3,volume=0.05"],
+  });
 
   console.log({"musicUrl": queue.get(msg.guild.id).songs[0].url, "musicTitle": queue.get(msg.guild.id).songs[0].title})
   if(aiclient){
